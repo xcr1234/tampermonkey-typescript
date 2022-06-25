e7f22c4cc900b965e95009999ba60e3460186336
xcr1234 <530551426@qq.com>
2022-06-25 20:39:32 +0800

    更新引入第三方模块例子

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
+// 引入第三方模块：
+// 例如引入lodash，在写代码时有代码提示
+import * as _ from 'lodash'
+
+
 console.log('hello world11123')
+// => { '4': 1, '6': 2 }
+console.log(_.countBy([6.1, 4.2, 6.3], Math.floor))
