22010a8168fc67cf86f714d54352c7e564bae201
xcr1234 <530551426@qq.com>
2022-06-25 20:42:56 +0800

    更新例子

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 // 引入第三方模块：
 // 例如引入lodash，在写代码时有代码提示
 import * as _ from 'lodash'
+// 引入项目中的文件：
+import Test from './test'
 
-
-console.log('hello world11123')
 // => { '4': 1, '6': 2 }
 console.log(_.countBy([6.1, 4.2, 6.3], Math.floor))
+
+const test = new Test()
+test.doTest()
